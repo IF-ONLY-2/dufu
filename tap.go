571a49c626a7748e0b309d09cb896cdba4d1f48d
Peng Gao <peng.gao.dut@gmail.com>
2017-08-02 01:39:13 -0700

    tcp: add ipv4 definition
    Signed-off-by: Peng Gao <peng.gao.dut@gmail.com>

--- tap.go
+++ tap.go
@@ -42,9 +42,9 @@ func NewTAP(name string) (*TapDevice, error) {
 	}
 	// req1 is a helper struct, to call SIOCGIFHWADDR ioctl
 	req1 := struct {
-		pad1             [0x12]byte
+		pad1            [0x12]byte
 		HardwareAddress [6]byte
-		pad2             [0x28 - 0x12 - 6]byte
+		pad2            [0x28 - 0x12 - 6]byte
 	}{}
 	// get MAC address
 	_, _, errno = syscall.Syscall(syscall.SYS_IOCTL, file.Fd(), uintptr(syscall.SIOCGIFHWADDR), uintptr(unsafe.Pointer(&req1)))

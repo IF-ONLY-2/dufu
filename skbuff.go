2e6d6ebf650249be3307fcd5877d43a8bbfe925f
Peng Gao <peng.gao.dut@gmail.com>
2017-08-08 02:10:14 -0700

    skbuff: add SkBuff struct
    SkBuff is a prependable buffer, it's like sk_buff in
    Linux network stack, method Prepend will leave a space
    at head, in order to construct lower layer headers.
    
    Signed-off-by: Peng Gao <peng.gao.dut@gmail.com>

---
+++ skbuff.go
@@ -0,0 +1,28 @@
+package dufu
+
+import (
+	"fmt"
+)
+
+// SkBuff is a buffer used for socket, it can be prepended from the head.
+type SkBuff struct {
+	buf   []byte
+	index int
+}
+
+func NewSkBuff(size int) *SkBuff {
+	return &SkBuff{
+		buf:   make([]byte, size),
+		index: size,
+	}
+}
+
+func (skb *SkBuff) Data() []byte {
+	fmt.Println("call Data, index", skb.index)
+	return skb.buf[skb.index:]
+}
+
+func (skb *SkBuff) Prepend(size int) {
+	fmt.Println("call Prepend, index", skb.index)
+	skb.index -= size
+}
